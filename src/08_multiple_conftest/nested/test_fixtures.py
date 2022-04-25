94bd2f12b2ebce7c5989871870590c708402501e
Nick <coffeebeforearch@gmail.com>
2022-04-24 23:48:33 -0500

    Add nested conftest example

---
+++ src/08_multiple_conftest/nested/test_fixtures.py
@@ -0,0 +1,14 @@
+# A simple example of test fixtures
+# By Nick from CoffeeBeforeArch
+
+# Simple function that squares a number
+def square(num):
+    return num * num
+
+
+# One test that uses our fixture
+def test_square(expensive_input):
+    for num in expensive_input:
+        result = square(num)
+        assert result == num ** 2
+
