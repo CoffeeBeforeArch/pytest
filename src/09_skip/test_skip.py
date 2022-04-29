951147c7cc5d7bb599511ff238c10d118df22586
Nick <coffeebeforearch@gmail.com>
2022-04-29 09:54:57 -0500

    Add param example to skip and xfail tests

--- src/09_skip/test_skip.py
+++ src/09_skip/test_skip.py
@@ -15,3 +15,12 @@ def test_square(expensive_input):
         result = square(num)
         assert result == num ** 31289421239
 
+# A parametrized test with one variant marked with skip
+@pytest.mark.parametrize('num', [
+    pytest.param(1),
+    pytest.param(429429243242, marks=pytest.mark.skip),
+])
+def test_multiple_square(num):
+    result = square(num)
+    assert result == num ** 3
+
