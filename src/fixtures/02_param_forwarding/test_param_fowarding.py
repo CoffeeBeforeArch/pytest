ac6c429e09ca7249a4f87b2541ace5a8edc80651
Nick <coffeebeforearch@gmail.com>
2022-05-17 08:09:44 -0500

    move example and minor refactor

--- src/fixtures/02_param_forwarding/test_param_fowarding.py
+++ src/fixtures/02_param_forwarding/test_param_fowarding.py
@@ -3,15 +3,17 @@
 
 import pytest
 
+@pytest.fixture(autouse=True)
+def log_num(num):
+    print(f"The value of num is {num}")
+
 # Simple function that squares a number
 def square(num):
     return num * num
 
-
 # One test that uses our fixture
 # The 'num_elements' parameter is automatically forwarded to our fixture
-@pytest.mark.parametrize("num_elements", [1, 2, 7, 10])
-def test_square(expensive_input, num_elements):
-    for num in expensive_input:
-        result = square(num)
-        assert result == num ** 2
+@pytest.mark.parametrize("num1", [1, 2, 3, 4, 5])
+def test_square(num1):
+    result = square(num1)
+    assert result == num1 ** 2
