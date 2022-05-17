2cd6d991bb30e20524c714637ba6b15c65b88705
Nick <coffeebeforearch@gmail.com>
2022-05-16 20:33:23 -0500

    Remove topics covered in earlier examples

--- src/fixtures/03_multiple_conftest/conftest.py
+++
@@ -1,9 +0,0 @@
-# conftest.py is a file that pytest will automatically import fixtures from
-
-import pytest
-import random
-
-# A simple figure that generates a random number
-@pytest.fixture
-def random_number():
-    return random.randint(0, 10)
--- src/fixtures/03_multiple_conftest/nested/conftest.py
+++
@@ -1,9 +0,0 @@
-# conftest.py is a file that pytest will automatically import fixtures from
-
-import pytest
-
-# A simple fixture that generates some inputs
-# This fixture uses another fixture which generates a random number
-@pytest.fixture
-def expensive_input(random_number):
-    return [i for i in range(random_number)]
