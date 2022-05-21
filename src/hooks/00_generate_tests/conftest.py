031bc722eda586f19cc6c389693d05a88409c75b
Nick <coffeebeforearch@gmail.com>
2022-05-21 18:04:34 -0500

    Minor refactor of dynamic parametrization test

--- src/hooks/00_generate_tests/conftest.py
+++ src/hooks/00_generate_tests/conftest.py
@@ -5,8 +5,4 @@ import pytest
 # A pytest hook to dynamically parametrize tests
 def pytest_generate_tests(metafunc):
     if "num" in metafunc.fixturenames:
-        entries = []
-        for i in range(10):
-            if i != 5:
-                entries.append(i)
-        metafunc.parametrize("num", entries)
+        metafunc.parametrize("num", [1, 2 ,3 ,4, 5])
