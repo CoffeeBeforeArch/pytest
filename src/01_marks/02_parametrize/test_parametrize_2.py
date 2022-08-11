9eb7e443324cb87b4c7d9ed4c10f97ea45f7f034
Nick <coffeebeforearch@gmail.com>
2022-08-11 09:30:55 -0500

    Change function name

--- src/01_marks/02_parametrize/test_parametrize_2.py
+++ src/01_marks/02_parametrize/test_parametrize_2.py
@@ -12,7 +12,7 @@ def pow(base, exponent):
 # Our test parametrized test
 @pytest.mark.parametrize("base", [1, 2, 3])
 @pytest.mark.parametrize("exponent", [4, 5, 6])
-def test_parametrize(base, exponent):
+def test_pow(base, exponent):
     # Test if our function matches math.pow
     result = pow(base, exponent)
     assert result == math.pow(base, exponent)
