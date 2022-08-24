5984a358d4e4bd791735d56ea32398915a21bd3a
Nick <coffeebeforearch@gmail.com>
2022-08-23 19:26:23 -0500

    re-structure tests

--- src/02_fixtures/02_param_forwarding/test_param_forwarding_0/conftest.py
+++ src/02_fixtures/02_param_forwarding/test_param_forwarding_0/conftest.py
@@ -5,5 +5,5 @@ import pytest
 # A simple fixture that generates some inputs
 # This takes one parameter that is forwarded from the test
 @pytest.fixture
-def expensive_input(num_elements):
+def element_list(num_elements):
     return [i for i in range(num_elements)]
