e165756a15b9b0548cee6f9157de08f7a8a0ba67
Nick <coffeebeforearch@gmail.com>
2022-08-25 09:23:40 -0500

    remove extra ex

--- src/02_fixtures/03_yield_fixture/conftest.py
+++ src/02_fixtures/03_yield_fixture/conftest.py
@@ -5,6 +5,6 @@ import pytest
 # A simple fixture that generates some inputs
 @pytest.fixture
 def initial_value():
-    print("Providing a value to our test!!")
+    print("Providing a value to our test!")
     yield 5
     print("Finishing Up!")
