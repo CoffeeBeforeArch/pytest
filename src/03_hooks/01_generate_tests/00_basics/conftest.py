4380dae309b5fe651331cddc5daa71ab7f2dd8f2
Nick <coffeebeforearch@gmail.com>
2022-08-25 20:19:32 -0500

    Minot dynamic param test updates

--- src/03_hooks/01_generate_tests/00_basics/conftest.py
+++ src/03_hooks/01_generate_tests/00_basics/conftest.py
@@ -4,5 +4,5 @@ import pytest
 
 # A pytest hook to dynamically parametrize tests
 def pytest_generate_tests(metafunc):
-    if "num" in metafunc.fixturenames:
-        metafunc.parametrize("num", [1, 2, 3, 4, 5])
+    if "initial_value" in metafunc.fixturenames:
+        metafunc.parametrize("initial_value", [1, 2, 3, 4, 5])
